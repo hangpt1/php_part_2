f9d1ba6154427bfe6e7a7c80123d7fbbbe9a4117
hangpt1 <phamhang1222004@gmail.com>
2024-08-26 11:02:27 +0700

    v1

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Trang của Hằng</title>
+    <link rel="stylesheet" href="./main.css">
+    
+</head>
+<body>
+<?php
+echo '<center><a target="_blank" href="https://www.facebook.com/profile.php?id=100013363294385&locale=vi_VN"><img class="img" src="./anh.jpg"></a></center>';
+echo '<center>
+        <p>Họ và tên: Phạm Thị Hằng</p>
+        <p>Lớp: K58SN2, Trường đại học Thương Mại</p>
+    <center>';
+echo 'Đây là commit';
+echo 'Đây là commit';
+echo '<button><a target="_blank"  href="https://github.com/hangpt1">Link Github</a></button>';
+?>
+</body>
+</html>
